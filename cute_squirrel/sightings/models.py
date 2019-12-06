06669c75ecf4b2421ce2d6d4ed77bd53a0567138
Chenqi Fan <cf2799@columbia.edu>
2019-12-06 22:04:16 +0000

    modigify model part one

--- cute_squirrel/sightings/models.py
+++ cute_squirrel/sightings/models.py
@@ -1,3 +1,76 @@
-from django.db import models
+from django.utils.translation import gettext as _
+
+class squirrel(models.Model):
+    Longitude = models.FloatField(
+        help_text=_('Longitude'),
+    )
+
+    Latitude = models.FloatField(
+        help_text=_('Latitude'),
+    )
+
+    Unique_Squirrel_ID = models.CharField(
+        help_text=_('Unique Squirrel ID'),
+        max_length=100,
+    )
+
+    AM = 'AM'
+    PM = 'PM'
+    OTHERS = ''
+
+    SHIFT_CHOICES = (
+        (AM, 'AM'),
+        (PM, 'PM'),
+        (OTHERS, ''),
+    )
+
+    Shift = models.CharField(
+        help_text=_('When the sighting session occured'),
+        max_length=2,
+        choices=SHIFT_CHOICES,
+        default=OTHERS,
+    )
+
+    Date = models.CharField(
+        help_text=_('The date of sighting session'),
+        max_length=10,
+    )
+
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+    OTHERS = ''
+
+    AGE_CHOICES = (
+        (ADULT, 'Adult'),
+        (JUVENILE, 'JUVENILE'),
+        (OTHERS, ''),
+    )
+
+    Age = models.CharField(
+        help_text=_('Whether a squirrel is an adult'),
+        max_length=10,
+        choices=AGE_CHOICES,
+        default=OTHERS,
+    )
+
+    GRAY = 'Gray'
+    CINNAMON = 'Cinnamon'
+    BLACK = 'Black'
+    OTHERS = ''
+
+    COLOR_CHOICES = (
+        (GRAY, 'Gray'),
+        (CINNAMON, 'Cinnamon'),
+        (BLACK, 'Black'),
+        (OTHERS, ''),
+    )
+
+    Primary_Fur_Color = models.CharField(
+        help_text=_('Primary fur color'),
+        max_length=10,
+        choices=COLOR_CHOICES,
+        default=OTHERS,
+    )
+
 
 # Create your models here.
