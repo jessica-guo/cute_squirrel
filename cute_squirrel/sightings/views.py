92d1af00b3c805fa88ce6aeae2633d88b4cdc99b
Chenqi Fan <cf2799@columbia.edu>
2019-12-06 22:19:20 +0000

    modify view.py -- all/update/add

--- cute_squirrel/sightings/views.py
+++ cute_squirrel/sightings/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import squirrel
+from .forms import SqForm
+
+def all_squirrel(request):
+    # A view to list all squirrels' unique id 
+    squirrels = squirrel.objects.all()
+    context = {
+        'squirrels':squirrels,
+    }
+    return render(request, 'sightings/all.html', context)
+
+def update_sightings(request,Unique_Squirrel_ID):
+    # A view to update a particular sighting
+    sighting = squirrel.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
+    if request.method == 'POST':
+        form = SqForm(request.POST, instance=sighting)
+        # check data is valid to post
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings')
+    else:
+        form = SqForm(instance=sighting)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'sightings/update.html', context)
+
+def add_sightings(request):
+    # A view to create a new sighting
+    if request.method == 'POST':
+        form = SqForm(request.POST)
+        # check data is calid to add
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings')
+    else:
+         form = SqForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'sightings/update.html', context)
 
 # Create your views here.
