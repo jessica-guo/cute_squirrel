131ae56fe9eb67bd92631fa2e1f3ae15fbd6eecd
Chenqi Fan <cf2799@columbia.edu>
2019-12-07 05:26:30 +0000

    add visualization function in views.py

--- cute_squirrel/sightings/urls.py
+++ cute_squirrel/sightings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.all_squirrel),
     path('add/', views.add_sightings),
     path('<Unique_Squirrel_ID>/', views.update_sightings),
+    path('stats/graph', views.graphs)
 ]
--- cute_squirrel/sightings/views.py
+++ cute_squirrel/sightings/views.py
@@ -4,6 +4,46 @@ from django.shortcuts import redirect
 from .models import squirrel
 from .forms import SqForm
 
+# the package used for stat
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import pandas as pd
+from matplotlib import pyplot as plt
+import io
+import matplotlib.pyplot as plt; plt.rcdefaults()
+
+def graphs(request):
+    fig = Figure()
+    canvas = FigureCanvas(fig)
+    squirrels = squirrel.objects.all()
+    columns = [field.name for field in squirrel._meta.fields]
+    df = pd.DataFrame(list(squirrels.values()),columns=columns)
+    colors = ['Pink','Salmon', 'Tomato']
+
+    plt_data_1 = df.groupby('Shift').count()['Unique_Squirrel_ID']
+    plt_data_2 = df.groupby('Age').count()['Unique_Squirrel_ID'][1:]
+    plt_data_3 = df.groupby('Primary_Fur_Color').count()['Unique_Squirrel_ID']
+    plt_data_4 = df.groupby('Location').count()['Unique_Squirrel_ID']
+    plt.figure(figsize=(12,12))
+    plt.subplot(2,2,1)
+    plt.pie(plt_data_1, labels=plt_data_1.index, labeldistance=1.1, shadow=False, startangle=None, pctdistance=0.6, autopct='%0.01f%%', colors=colors)
+    plt.title('When the sightings happen')
+    plt.subplot(2,2,2)
+    plt.pie(plt_data_2, labels=plt_data_2.index, labeldistance=1.1, shadow=False, startangle=None, pctdistance=0.6, autopct='%0.01f%%', colors=colors)
+    plt.title('The age of squirrels')
+    plt.subplot(2,2,3)
+    plt.pie(plt_data_3, labels=plt_data_3.index, labeldistance=1.1, shadow=False, startangle=None, pctdistance=0.6, autopct='%0.01f%%',colors=colors)
+    plt.title('Primary Fur color of squirrels')
+    plt.subplot(2,2,4)
+    plt.pie(plt_data_4, labels=plt_data_4.index, labeldistance=1.1, shadow=False, startangle=None, pctdistance=0.6, autopct='%0.01f%%',colors=colors)
+    plt.title('The location of squirrels')
+    
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close(fig)
+    response = HttpResponse(buf.getvalue(),content_type='image/png')
+    return response
+
 def all_squirrel(request):
     # A view to list all squirrels' unique id 
     squirrels = squirrel.objects.all()
