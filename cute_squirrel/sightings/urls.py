23395c61f9e73d0b164d603c1dfd641a2fc6bfd7
jessica-guo <pg2651@columbia.edu>
2019-12-07 06:45:55 +0000

    create stats functions in views.py, stats.html, urls.py

--- cute_squirrel/sightings/urls.py
+++ cute_squirrel/sightings/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('stats/',views.stats),
     path('', views.all_squirrel),
     path('add/', views.add_sightings),
     path('<Unique_Squirrel_ID>/', views.update_sightings),
--- cute_squirrel/sightings/views.py
+++ cute_squirrel/sightings/views.py
@@ -3,6 +3,34 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from .models import squirrel
 from .forms import SqForm
+import pandas as pd
+
+def stats(request):
+    squirrels = squirrel.objects.all()
+    columns = [field.name for field in squirrel._meta.fields]
+    AM=pd.DataFrame(list(squirrels.values())).groupby('Shift').count()['Unique_Squirrel_ID'][0]
+    PM=pd.DataFrame(list(squirrels.values())).groupby('Shift').count()['Unique_Squirrel_ID'][1]
+    Adult=pd.DataFrame(list(squirrels.values())).groupby('Age').count()['Unique_Squirrel_ID'][0]
+    Juvenile=pd.DataFrame(list(squirrels.values())).groupby('Age').count()['Unique_Squirrel_ID'][1]
+    Grey=pd.DataFrame(list(squirrels.values())).groupby('Primary_Fur_Color').count()['Unique_Squirrel_ID'][0]
+    Cinnamon=pd.DataFrame(list(squirrels.values())).groupby('Primary_Fur_Color').count()['Unique_Squirrel_ID'][1]
+    Black=pd.DataFrame(list(squirrels.values())).groupby('Primary_Fur_Color').count()['Unique_Squirrel_ID'][2]
+    Ground_Plane=pd.DataFrame(list(squirrels.values())).groupby('Location').count()['Unique_Squirrel_ID'][0]
+    Above_Ground=pd.DataFrame(list(squirrels.values())).groupby('Location').count()['Unique_Squirrel_ID'][1]
+
+    context = {
+            'AM':AM,
+            'PM':PM,
+            'Adult':Adult,
+            'Juvenile':Juvenile,
+            'Grey':Grey,
+            'Cinnamon':Cinnamon,
+            'Black':Black,
+            'Ground_Plane':Ground_Plane,
+            'Above_Ground':Above_Ground,
+            }
+    return render(request, 'sightings/stats.html',context)
+
 
 def all_squirrel(request):
     # A view to list all squirrels' unique id 
